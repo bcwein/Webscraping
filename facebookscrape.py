3bd686bcdec4aaa70982d2642935ff154e76713e
bcwein <bcwein343@gmail.com>
2021-06-04 10:37:40 +0200

    Webscrape code version 1.

---
+++ facebookscrape.py
@@ -0,0 +1,58 @@
+"""
+Webscraping of companies for extracting facebook post stats.
+
+This module extracts:
+    - Post date
+    - No of likes
+    - No of comments
+
+And it returns a dataframe of companies and their posts.
+
+All data input in this code is seperated into yaml files not included in the
+repository.
+"""
+# %%
+from facebook_scraper import get_posts
+import pandas as pd
+from multiprocessing import Pool
+import time
+from datetime import timedelta
+import yaml
+
+
+def dataFrameCreatorFacebook(company, pages=10):
+    """Construct Pandas dataframe for facebook page.
+
+    Args:
+        company (string): url identifier for facebook page
+        pages (int): No of pages to scroll until termination.
+    Returns:
+        pandas dataframe: dataframe of company, post, like and comments
+    """
+    data = []
+    for post in get_posts(company, pages=pages, options={"comments": True},
+                          cookies="cookies.txt"):
+        try:
+            data.append(
+                [company, post['time'], post['likes'], post['comments']]
+            )
+        except Exception as ex:
+            continue
+    return pd.DataFrame(data, columns=["site", "time", "likes", "comments"])
+
+
+# %%
+if __name__ == '__main__':
+    """Main function.
+
+    This main function does the following:
+        1. Load input data from data.yml
+        2. Create a list of dataframes with list comprehension.
+        3. Concatenate Dataframes.
+        4. Save dataframe to csv.
+    """
+    data = yaml.load(open('data.yml'), Loader=yaml.FullLoader)
+    sites = data['sites-transport']
+    result = [dataFrameCreatorFacebook(site, pages=100) for site in sites]
+    parsed = pd.concat(result)
+    parsed.to_csv('parsed-transport.csv')
